a689aab3c9316e4420a9ea7fa837591d557f0ee5
José Ángel Sánchez <gelanchez@gmail.com>
2020-09-20 13:47:23 +0100

    Initial main webserver and name changes

---
+++ src/webserver.ino
@@ -0,0 +1,29 @@
+/**
+ * @file webserver.ino
+ * @author José Ángel Sánchez (https://github.com/gelanchez)
+ * @brief Main implementation of the webserver to control a LED and present
+ * data from the sensors to the clients. Two webservers and technologies can
+ * be used:
+ * a) Simple webserver + jQuery AJAX.
+ * b) Async webserver + websockets.
+ * @version 0.0.1
+ * @date 2020-09-20
+ * @copyright GPL-3.0
+ */
+
+#ifndef ESP32
+#define ESP32
+
+#include <ArduinoJson.h>
+#include "constants.h"
+#include <ESPAsyncWebServer.h>
+#include "index.h"  // HTML webpage contents with javascripts
+#include "mysensors.h"
+#include "utils.h"
+#include <WebServer.h>
+
+
+
+
+
+#endif
